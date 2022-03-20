b9a68adf5d0d8d13d7296e8932816d68abc0be12
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2022-03-20 16:07:14 -0400

    updated signup

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { Link, useLocation  } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import {auth} from './../../firebase/utils'
+
 
 import './style.scss';
 
 const Header = props => {
+    const {currentUser} = props;
     return (
         <header className="header">
         <div className="wrap">
-            
             <Link to="/">
             <h1>E-Shop</h1>
             </Link>
             <div className="callToActions">
-                <ul>
+                {currentUser && (
+                    <ul><li><span onClick={()=> auth.signOut()}>
+                        Logout
+                        </span></li></ul>
+                )}
+                {!currentUser && (
+                    <ul>
                     <li>
                         <Link to="/registration">
                             Signup
@@ -24,6 +32,7 @@ const Header = props => {
                         </Link>
                     </li>
                 </ul>
+                )}
             </div>
 
 
@@ -33,6 +42,10 @@ const Header = props => {
 
         </header>
     );
+};
+
+Header.defaultProps = {
+    currentUser: null
 };
 
 export default Header;
--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 import { firebaseConfig } from './config';
 
 firebase.initializeApp(firebaseConfig);
@@ -9,41 +9,32 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 export const GoogleProvider = new firebase.auth.GoogleAuthProvider();
-GoogleProvider.setCustomParameters({ prompt: 'select_account' });
+GoogleProvider.setCustomParameters({promt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(GoogleProvider);
 
-export const handleUserProfile = async ({ userAuth, additionalData }) => {
-  if (!userAuth) return;
-  const { uid } = userAuth;
-
-  const userRef = firestore.doc(`users/${uid}`);
-  const snapshot = await userRef.get();
-
-  if (!snapshot.exists) {
-    const { displayName, email } = userAuth;
-    const timestamp = new Date();
-    const userRoles = ['user'];
-
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdDate: timestamp,
-        userRoles,
-        ...additionalData
-      });
-    } catch(err) {
-      // console.log(err);
+export const handleUserProfile = async ( userAuth, additionalData ) => {
+    if (!userAuth) return;
+    const { uid } = userAuth;
+  
+    const userRef = firestore.doc(`users/${uid}`);
+    const snapshot = await userRef.get();
+  
+    if (!snapshot.exists) {
+      const { displayName, email } = userAuth;
+      const timestamp = new Date();
+      const userRoles = ['user'];
+  
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdDate: timestamp,
+          userRoles,
+          ...additionalData
+        });
+      } catch(err) {
+        // console.log(err);
+      }
     }
-  }
-
-  return userRef;
-};
-
-export const getCurrentUser = () => {
-  return new Promise((resolve, reject) => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
-      unsubscribe();
-      resolve(userAuth);
-    }, reject);
-  })
-}
+    return userRef;
+  };
